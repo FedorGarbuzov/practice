b530d0aab079e0b4cd3a2637bb7462500517db09
Федор Гарбузов <71636417+FedorGarbuzov@users.noreply.github.com>
2021-10-19 13:33:10 +0300

    rxjava_practice

--- app/src/main/java/ru/surf/practice/MainActivity.kt
+++ app/src/main/java/ru/surf/practice/MainActivity.kt
@@ -5,6 +5,8 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import rx.Observable
+import rx.Subscriber
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,6 +33,141 @@ class MainActivity : AppCompatActivity() {
 
         button.setOnClickListener {
             activityLauncher.launch("I’ll be back")
+        }
+        getProductsById()
+    }
+
+    data class Product(
+        val id: Int,
+        val categoryId: Int,
+        val name: String
+    )
+
+    data class Category(
+        val id: Int,
+        val name: String,
+        val subCategory: List<SubCategory>
+    ) {
+        data class SubCategory(
+            val id: Int,
+            val categoryId: Int,
+            val name: String
+        )
+    }
+
+
+    val pork = Product(
+        1,
+        1,
+        "Pork"
+    )
+
+    val chicken = Product(
+        2,
+        1,
+        "Chicken"
+    )
+
+    val beef = Product(
+        3,
+        1,
+        "Beef"
+    )
+
+    val gouda = Product(
+        4,
+        1,
+        "Gouda"
+    )
+
+    val parmesan = Product(
+        5,
+        1,
+        "Parmesan"
+    )
+
+    val hat = Product(
+        6,
+        2,
+        "Hat"
+    )
+
+    val jacket = Product(
+        7,
+        2,
+        "Jacket"
+    )
+
+    val pants = Product(
+        8,
+        2,
+        "Pants"
+    )
+    val socks = Product(
+        9,
+        2,
+        "Socks"
+    )
+
+
+    val meatSubCategory = Category.SubCategory(
+        1,
+        1,
+        "Meat"
+    )
+
+    val cheeseSubCategory = Category.SubCategory(
+        2,
+        1,
+        "Cheese"
+    )
+
+    val outerwearSubCategory = Category.SubCategory(
+        3,
+        2,
+        "Outerwear"
+    )
+
+    val lingerieSubCategory = Category.SubCategory(
+        4,
+        2,
+        "Lingerie"
+    )
+
+    val foodCategory = Category(
+        1,
+        "Food",
+        listOf(meatSubCategory, cheeseSubCategory)
+    )
+
+    val wearCategory = Category(
+        2,
+        "Wear",
+        listOf(outerwearSubCategory, lingerieSubCategory)
+    )
+
+    val products = Observable.just(pork, chicken, beef, gouda, parmesan, hat, jacket, pants, socks)
+    val productCategories = Observable.just(foodCategory, wearCategory)
+
+    fun getProductsById() {
+
+        val categoryId = 1
+
+        val ids = productCategories.filter {
+            it.id == categoryId
+        }.map { category ->
+            category.subCategory.map {
+                it.categoryId
+            }
+        }
+
+
+        val products = ids.map { list ->
+            list.map { id ->
+                products.filter { product ->
+                    product.id == id
+                }
+            }
         }
     }
 }
